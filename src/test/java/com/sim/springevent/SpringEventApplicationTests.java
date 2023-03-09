e3b445a2d496dcacb8f05a2ff20f6d1b18669e1b
tlarbals824 <simkm0824@naver.com>
2023-03-09 17:26:34 +0900

    docs : first setting

---
+++ src/test/java/com/sim/springevent/SpringEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sim.springevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
